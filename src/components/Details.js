519a2f2322d5457ea17328f51039b2c5955aadba
Devkant <devkant21@gmail.com>
2023-05-03 22:58:02 +0530

    fix: form

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,26 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Details = () => {
+  const[user, setUser] = useState({
+    name: "",
+    telephone: "",
+    email: "",
+
+
+  });
+    const getUserData = (event) => {
+
+    };
   return (
     <React.Fragment>
         <div className='text-6xl font-bold p-4 mx-4 mt-4'>
             Enter your Details
             <div className='text-3xl mt-6 pt-6 font-serif'>
-              <form>
+
+
+
+              <form className='text-4xl font-light'>
                 <label>Name
-                  <input type="text" />
+                  <input
+                   type="text"
+                   name="name"
+                   placeholder="Enter your Name"
+                   value={user.name}
+                   onChange={getUserData}
+                   autocomplete="off"required 
+                  />
                 </label>
-              </form>
-              <form>
+              
                 <label>Telephone
-                  <input type="text" />
+                <input
+                   type="text"
+                   name="telephone"
+                   placeholder="Telephone number"
+                   value={user.telephone}
+                   onChange={getUserData}
+                   autocomplete="off"required 
+                  />
+
+                </label>
+                <label>Email
+                <input
+                   type="text"
+                   email="email"
+                   placeholder="Email address"
+                   value={user.email}
+                   onChange={getUserData}
+                   autocomplete="off"required 
+                  />
+                
                 </label>
               </form>
-              <form>
+              {/* <form>
                 <label>Name
                   <input type="text" />
                 </label>
-              </form> 
+              </form>  */}
             </div>
         </div>
     </React.Fragment>
