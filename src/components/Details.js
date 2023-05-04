da4989cade9157eb96fa41b24da2342020d908a4
Devkant <devkant21@gmail.com>
2023-05-04 23:42:32 +0530

    fix :object destrucing

--- src/components/Details.js
+++ src/components/Details.js
@@ -15,17 +15,33 @@ const Details = () => {
       setUser({ ...user, [name]: value });
     };
 
-    const postData = (e) => {
+    const postData = async(e) => {
       e.preventDefault();
-    }
+
+        const {
+          name,
+          telephone,
+          email,
+        } = user;
+
+      const res = await fetch("https://details-extraction-default-rtdb.firebaseio.com/userData.json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          telephone,
+          email,
+        }),
+      }
+    );
+  };
   return (
     <React.Fragment>
         <div className='text-6xl font-bold p-4 mx-4 mt-4'>
             Enter your Details
             <div className='text-3xl mt-6 pt-6 font-serif'>
-
-
-
               <form className='text-4xl font-light' method="POST">
                 <span className='text-8xl justify-between'>Name
                   <input
