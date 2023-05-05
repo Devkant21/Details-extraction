bf15e79306b78bad6e31d1592e501f7642b83a67
Devkant <devkant21@gmail.com>
2023-05-05 20:07:15 +0530

    fix: data extraction

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,6 +5,7 @@ const Details = () => {
     name: "",
     telephone: "",
     email: "",
+    day: "",
   });
 
   let name, value;
@@ -18,13 +19,16 @@ const Details = () => {
     const postData = async(e) => {
       e.preventDefault();
 
+
         const {
           name,
           telephone,
           email,
+          day,
         } = user;
 
-      const res = await fetch("https://details-extraction-default-rtdb.firebaseio.com/userData.json", {
+        if (name && telephone && email && day) {
+          const res = await fetch("https://details-extraction-default-rtdb.firebaseio.com/userData.json", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -33,66 +37,82 @@ const Details = () => {
           name,
           telephone,
           email,
+          day,
         }),
       }
-    );
-
-    if (res) {
+    ); if (res) {
       setUser({
         name: "",
         telephone : "",
         email : "",
+        day : "",
       });
+      alert("Data stored successfully ");
     }
+        }
+          else {
+            alert("Please fill all the datas");
+          } 
   };
   return (
     <React.Fragment>
         <div className='text-6xl font-bold p-4 mx-4 mt-4'>
-            Enter your Details
+            Extraction : And here I collect your datas
             <div className='text-3xl mt-6 pt-6 font-serif'>
               <form className='text-4xl font-light' method="POST">
-                <span className='text-8xl justify-between'>Name
-                  <input
-                   type="text"
-                   name="name"
-                   placeholder="Enter your Name"
-                   value={user.name}
-                   onChange={getUserData}
-                   autocomplete="off"
-                   required 
+                <span className='text-4xl justify-between border-spacing-4'>Name
+                    
+                  <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe'
+                    type="text"
+                    name="name"
+                    placeholder="Enter your Name"
+                    value={user.name}
+                    onChange={getUserData}
+                    autocomplete="off"
+                    required 
                   />
                 </span>
               
-                <span className="text-4xl">Telephone
-                <input
-                   type="text"
-                   name="telephone"
-                   placeholder="Telephone number"
-                   value={user.telephone}
-                   onChange={getUserData}
-                   autocomplete="off"
-                   required 
-                />
+                <span className="text-4xl mt-10">Telephone
+                  <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="00000 00000'
+                    type="text"
+                    name="telephone"
+                    placeholder="Telephone number"
+                    value={user.telephone}
+                    onChange={getUserData}
+                    autocomplete="off"
+                    required 
+                  />
                 </span>
-                <span className="text-4xl">Email
-                <input
-                   type="text"
-                   email="email"
-                   placeholder="Email address"
-                   value={user.email}
-                   onChange={getUserData}
-                   autocomplete="off"
-                   required 
-                />
-                
+
+                <span className="text-4xl mt-4">Email
+                  <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe"'
+                    type="text"
+                    name="email"
+                    placeholder="Email address"
+                    value={user.email}
+                    onChange={getUserData}
+                    autocomplete="off"
+                    required 
+                  />
+                </span>
+                <span className="text-4xl mt-4">How was your day today?
+                  <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe"'
+                    type="text"
+                    name="day"
+                    placeholder="awesome"
+                    value={user.day}
+                    onChange={getUserData}
+                    autocomplete="off"
+                    required 
+                  />
                 </span>
                 <div className='p-4 rounded-md bg-blend-color outline-teal-900 hover
                 blue'>
-                  <button onClick={postData}>
+                  <button className='shadow bg-gradient-to-r from-indigo-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-gray-100 font-bold py-2 px-4 rounded" type="button"' onClick={postData}>
                     <span>
                       Submit
                     </span>
-
                   </button>
                 </div>
               </form>
